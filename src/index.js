d5e42e0bb766a2f6698cef0a84aebf82dcd90832
Olga <revina.volga@gmail.ru>
2019-02-25 17:27:02 +0300

    feat: add completed task to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+  let objCoins = {H: 50, Q: 25, D: 10, N: 5, P: 1};
+  let objMake = {};
+
+  if (currency === 0) {
+    objMake = {};
+    return objMake;
+  }
+
+  if (currency > 10000) {
+    objMake.error = "You are rich, my friend! We don't have so much coins for exchange";
+    return objMake;
+  }
+
+  for (key in objCoins) {
+    if (currency / objCoins[key] >= 1) {
+      objMake[key] = Math.floor(currency / objCoins[key]);
+      currency = currency % objCoins[key];
+    }
+  }
+
+  return objMake;
 }
